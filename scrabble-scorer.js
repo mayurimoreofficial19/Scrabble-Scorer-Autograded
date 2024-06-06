a1958d6b65c7c842d366c2dd1d6439faba7a1fc3
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-06-06 17:03:50 -0500

    task-2 is done

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,7 +1,8 @@
 // This assignment is inspired by a problem on Exercism (https://exercism.org/tracks/javascript/exercises/etl) that demonstrates Extract-Transform-Load using Scrabble's scoring system. 
 
 const input = require("readline-sync");
-let inputWord = '';
+let userInput = '';
+//let numericalScore = 0;
 
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
@@ -34,30 +35,89 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! ");
-   inputWord = input.question("Enter a word to score: ");
+   console.log("\nLet's play some scrabble! \n");
+   userInput = input.question("Enter a word to score: ");
 };
 
 let newPointStructure;
 
 let simpleScorer = function(word)
 {
-      
+      word = word.toUpperCase();
+      let numericalScore = 0;
+
+      for(let Index = 0; Index < word.length; Index++)
+         {
+            numericalScore += 1;
+         }
+         return `Score for '${word}' : ${numericalScore}`;
 };
 
-let vowelBonusScorer;
+let vowelBonusScorer = function(word)
+{
+   word = word.toUpperCase();
+   let numericalScore = 0;
+
+   for(let Index = 0; Index < word.length; Index++)
+      {
+         if(word[Index] == 'A' || word[Index] == 'E' || word[Index] == 'I' || word[Index] == 'U' || word[Index] == ')')
+            {
+               numericalScore += 3;
+            }
+            else
+            {
+               numericalScore += 1;
+            }
+      }
+      return `Score for '${word}' : ${numericalScore}`;
+};
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+let simpleScore = {
+ 'name': 'Simple Score',
+ 'description': 'Each letter is worth 1 point.',
+ 'scoringFunction': simpleScorer
+};
 
-function scorerPrompt() {}
+let bonusVowel = {
+'name': 'Bonus Vowels',
+'description': 'Vowels are 3 pts, consonants are 1 pt.',
+'scoringFunction': vowelBonusScorer
+};
+
+let scrabble = {
+'name': 'Scrabble',
+'description': 'The traditional scoring algorithm.',
+'scoringFunction': oldScrabbleScorer
+};
+
+const scoringAlgorithms = [simpleScore,bonusVowel,scrabble];
+
+function scorerPrompt() {
+   console.log(' Which scoring algorithm would you like to use?\n');
+
+   for(let Index = 0; Index < scoringAlgorithms.length; Index++)
+      {
+         console.log(`${Index} - ${scoringAlgorithms[Index].name} : ${scoringAlgorithms[Index].description}`);
+         //console.log(`Scoring Function Result : ${scoringAlgorithms[Index].scoringFunction}`);
+      }
+      let selectOption = input.question('Enter 0, 1 or 2 : ');
+      let scoringFunction = scoringAlgorithms[selectOption].scoringFunction;
+
+      console.log(scoringFunction(userInput));
+
+      return scoringAlgorithms;
+}
 
 function transform() {};
 
 function runProgram() {
    initialPrompt();
-   console.log(oldScrabbleScorer(inputWord));
+   // console.log(oldScrabbleScorer(userInput));
+   // console.log(simpleScorer(userInput));
+   // console.log(vowelBonusScorer(userInput));
+   scorerPrompt();
 }
 
 //runProgram();
